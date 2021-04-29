432306728715c793c69f9c7fe95f0aa38d3c9268
unknown <hudsoncratz@gmail.com>
2021-04-29 20:01:37 -0300

    integrated application  with docker

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:alpine
+
+WORKDIR /usr/app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+EXPOSE 4040
+
+CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,81 +1,86 @@
 const express = require('express');
-const models = require('./models')
-const sequelize = require('sequelize');
-const status = require('./helpers/http-status')
+// const models = require('./models')
+// const sequelize = require('sequelize');
+// const status = require('./helpers/http-status')
 const cors = require("cors")
-const mysql = require('mysql');
+// const mysql = require('mysql');
+
 const port = 4040;
+const host = '0.0.0.0';
+
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('Hello Day')
+})
+
+app.listen(port, host);
+
+// app.use(cors())
+// app.use(express.json())
+
+// /**
+//  * ROUTES
+//  */
+// const CitizenRoutes = require("./routes/CitizenRoutes")
+// app.use('/citizen', CitizenRoutes)
+
+// const AllergyRoutes = require('./routes/AllergyRoutes')
+// app.use('/alergy', AllergyRoutes)
+
+// const HealthPlanRoutes = require('./routes/HealthPlanRoutes')
+// app.use('/healtPlan', HealthPlanRoutes)
+
+// const ReligionRoutes = require('./routes/ReligionRoutes')
+// app.use('/religion', ReligionRoutes)
+
+// const VaccineRoutes = require('./routes/VaccineRoutes');
+// const { response } = require('express');
+// app.use('/vaccine', VaccineRoutes)
+
 
 /**
- * ROUTES
+ * CONNECTION MYSQL-DOCKER
  */
-const CitizenRoutes = require("./routes/CitizenRoutes")
-app.use('/citizen', CitizenRoutes)
 
-const AllergyRoutes = require('./routes/AllergyRoutes')
-app.use('/alergy', AllergyRoutes)
 
-const HealthPlanRoutes = require('./routes/HealthPlanRoutes')
-app.use('/healtPlan', HealthPlanRoutes)
+// var mysqlHost = process.env.MYSQL_HOST || 'localhost';
+//     var mysqlPort = process.env.MYSQL_PORT || '3306';
+//     var mysqlUser = process.env.MYSQL_USER || 'root';
+//     var mysqlPass = process.env.MYSQL_PASS || 'root';
+//     var mysqlDB = process.env.MYSQL_DB || 'vacinapp';
 
-const ReligionRoutes = require('./routes/ReligionRoutes')
-app.use('/religion', ReligionRoutes)
+//     var connectionOptions = {
+//         host: mysqlHost,
+//         port: mysqlPort,
+//         user: mysqlUser,
+//         password: mysqlPass,
+//         database: mysqlDB
+//     };
 
-const VaccineRoutes = require('./routes/VaccineRoutes');
-const { response } = require('express');
-app.use('/vaccine', VaccineRoutes)
+//     console.log('MySQL Connection config:');
+//     console.log(connectionOptions);
 
-app.listen(port, function (err) {
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", port);
+//     var connection = mysql.createConnection(connectionOptions);
+//     var queryStr = "SELECT * FROM MOE_ITEM_T";
 
-    /**
-     * CONNECTION MYSQL-DOCKER
-     */
-    app.get('/', function (req, res) {
-        var mysqlHost = process.env.MYSQL_HOST || 'localhost';
-        var mysqlPort = process.env.MYSQL_PORT || '3306';
-        var mysqlUser = process.env.MYSQL_USER || 'root';
-        var mysqlPass = process.env.MYSQL_PASS || 'root';
-        var mysqlDB = process.env.MYSQL_DB || 'vacinapp';
+//     connection.connect();
 
-        var connectionOptions = {
-            host: mysqlHost,
-            port: mysqlPort,
-            user: mysqlUser,
-            password: mysqlPass,
-            database: mysqlDB
-        };
+//     connection.query(queryStr, function (error, results, fields) {
+//         if (error) throw error;
 
-        console.log('MySQL Connection config:');
-        console.log(connectionOptions);
+//         responseStr = '';
 
-        var connection = mysql.createConnection(connectionOptions);
-        var queryStr = "SELECT * FROM MOE_ITEM_T";
+//         results.forEach(function (data) {
+//             responseStr += data.ITEM_NAME + ' : '
+//             console.log(data)
+//         })
 
-        connection.connect();
+//         if (responseStr.length == 0)
+//             responseStr = 'No records found'
 
-        connection.query(queryStr, function (error, results, fields) {
-            if (error) throw error;
+//         console.log(responseStr)
 
-            responseStr = '';
-
-            results.forEach(function (data) {
-                responseStr += data.ITEM_NAME + ' : '
-                console.log(data)
-            })
-
-            if (responseStr.length == 0)
-                responseStr = 'No records found'
-
-            console.log(responseStr)
-
-            res.status(status.success).send(responseStr)
-        })
-        connection.end()
-    })
-})
+//         res.status(status.success).send(responseStr)
+//     })
+//     connection.end()
