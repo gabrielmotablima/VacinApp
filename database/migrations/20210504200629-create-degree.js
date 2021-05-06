6fdd782baffbfc4e60a1efed537224b7834a2c42
Guilherme <guilherme.steink@gmail.com>
2021-05-06 00:16:55 -0300

    fix

---
+++ database/migrations/20210504200629-create-degree.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('degrees', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      level: {
+        type: Sequelize.STRING(45),
+        allowNull: false
+      },
+      citizenSusNumber: {
+        type: Sequelize.INTEGER(5),
+        allowNull: false,
+        references: {
+          model: 'citizens', 
+          key: 'susNumber'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('degrees'); 
+  }
+};
